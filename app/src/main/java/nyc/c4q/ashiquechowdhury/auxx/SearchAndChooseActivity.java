f250278452aef784aa6210f2ce34cda36fb863f9
CurrentlyJordan <jwsmith451@gmail.com>
2017-03-05 17:54:33 -0500

    working textwatcher

--- app/src/main/java/nyc/c4q/ashiquechowdhury/auxx/SearchAndChooseActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/auxx/SearchAndChooseActivity.java
@@ -2,14 +2,16 @@ package nyc.c4q.ashiquechowdhury.auxx;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
@@ -58,11 +60,10 @@ public class SearchAndChooseActivity extends AppCompatActivity implements
     Button queueButton;
     Button nextButton;
     Button previousButton;
-    TextView playingTitle;
-    TextView playingArtist;
-    TextView playingAlbum;
     public static int trackCounter = 0;
     public static List<String> trackList = new ArrayList<>();
+    private long lastChange = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,8 +126,46 @@ public class SearchAndChooseActivity extends AppCompatActivity implements
             }
         });
 
-
+        editText.addTextChangedListener(searchWatcher);
     }
+
+    private final TextWatcher searchWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            final String searchTerm = s.toString();
+            new Handler().postDelayed(
+
+                    new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (noChangeInText()) {
+                                getSongData(searchTerm);
+                            }
+                        }
+                    },
+                    300);
+
+            lastChange = System.currentTimeMillis();
+
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+
+        private boolean noChangeInText() {
+            return System.currentTimeMillis() - lastChange >= 300;
+        }
+    };
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
