6aa205cb9d2e8a6dd18ca2814d6f806bb97716ac
sderekj <dereksantos1989@gmail.com>
2017-03-05 02:01:51 -0500

    Fixed EditText Attributes

--- app/src/main/java/nyc/c4q/ashiquechowdhury/auxx/LoginActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/auxx/LoginActivity.java
@@ -1,9 +1,15 @@
 package nyc.c4q.ashiquechowdhury.auxx;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -13,6 +19,9 @@ import com.bumptech.glide.Glide;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText usernameEditText;
+    EditText passwordEditText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,50 @@ public class LoginActivity extends AppCompatActivity {
 
         ImageView logoIv = (ImageView) findViewById(R.id.aux_logo);
         Glide.with(getApplicationContext()).load(R.drawable.lasso4).into(logoIv);
+
+        CardView auxSignInButton = (CardView) findViewById(R.id.aux_signin_button);
+        CardView googleSignInButton = (CardView) findViewById(R.id.google_signin_button);
+        usernameEditText = (EditText) findViewById(R.id.username_login_edittext);
+        passwordEditText = (EditText) findViewById(R.id.password_login_edittext);
+
+
+        auxSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Add Intent to Next Activity", Toast.LENGTH_SHORT).show();
+
+                String enteredUsername = getUsernameText();
+                String enteredPassword = getPasswordText();
+
+                handleLogin(enteredUsername, enteredPassword);
+            }
+        });
+
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Hangle Google Sign In", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleLogin(String enteredUsername, String enteredPassword) {
+        if (enteredUsername.isEmpty()) {
+            Toast.makeText(this, "Enter a username", Toast.LENGTH_SHORT).show();
+        } else if (enteredPassword.isEmpty()) {
+            Toast.makeText(this, "Enter a password", Toast.LENGTH_SHORT).show();
+        } else {
+//            Intent intent = new Intent(getApplicationContext(), JoinRoomActivity.class);
+//            startActivity(intent);
+        }
+    }
+
+    private String getUsernameText() {
+        return usernameEditText.getText().toString();
+    }
+
+    private String getPasswordText() {
+        return passwordEditText.getText().toString();
     }
 }
